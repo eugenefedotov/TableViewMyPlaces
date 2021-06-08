00033660ceff2ee56d139f8919b11341c4febef2
Fedotov Eugene <ea.fedotov@severmed.com>
2021-06-08 16:44:40 +0300

    Initial project

--- TableViewMyPlaces/ViewController.swift
+++ TableViewMyPlaces/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let restaurantNames = [
+            "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+            "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+            "Speak Easy", "Morris Pub", "Вкусные истории",
+            "Классик", "Love&Life", "Шок", "Бочка"
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else { fatalError() }
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        return cell
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
 
 }
 
